3d2692057e0f79351e4200571064b072e1ab4652
marvrod <marcelovvilla@gmail.com>
2022-05-29 17:40:02 -0500

    Complete exercise 1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,32 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+ 
+ let word = "";
+ 
+ if (string === "") {
+     return "";
+ }
+ 
+ else if (number < 0) {
+    return "ERROR";
+}
 
+else {
+ for (let i = 0; i <= number; i++) {
+
+
+    if (i == 0) {
+        word = word;
+    }
+    else if (i > 0) {
+        word = word + string;
+    }
+    else {
+        return "Invalid input";
+    }
+};
+
+ return  word;
+}
 };
 
 // Do not edit below this line
