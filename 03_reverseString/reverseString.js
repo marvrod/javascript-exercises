582cc9c3b7ddb540923dba1b7ef75bdb4df39237
marvrod <marcelovvilla@gmail.com>
2022-05-29 18:55:25 -0500

    Solve exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
+const reverseString = function(text) {
+ let textArray = text.split("");
+ let length = textArray.length;
+ let newText= "";
 
+ if (text === "") {
+     return "";
+ }
+ else {
+ for (let i = length - 1; i >= 0; i--) {
+    newText = newText + textArray[i];
+ }
+
+return newText;
 };
+}
 
 // Do not edit below this line
 module.exports = reverseString;
