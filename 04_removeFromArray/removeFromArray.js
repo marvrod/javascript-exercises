ddcd979259fdc6cb84c240575eb93c62817ac620
marvrod <marcelovvilla@gmail.com>
2022-05-29 20:09:37 -0500

    Solve exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(array1, ...elements) {
+ let length1 = array1.length;
+ let length2 = elements.length;
+ let newArray = array1;
 
+
+ // First for loop for checkin each element in the rest parameter
+ for (let i = 0; i <= length2 - 1; i++) {
+    let element = elements[i];
+    // Second for loop for comparing it to each element in the array1
+    for (let j = 0; j <= length1 - 1; j++) {
+        if (elements[i] === newArray[j]) {
+            newArray.splice(j, 1);
+        }
+        else {
+            newArray = newArray;
+        }
+    }
+ }
+ return newArray;
 };
 
 // Do not edit below this line
